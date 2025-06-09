1cd6bce6ed18c5e0c78b41d9cccd44c8d7953b89
Denis <denisdolmatov2020@gmail.com>
2025-06-09 15:37:28 +0700

    Update RoutePage.vue

--- src/components/core/RoutePage.vue
+++ src/components/core/RoutePage.vue
@@ -277,7 +277,7 @@ onMounted(() => {
             </div>
           </div>
           <iframe 
-            src="/chat?hideMenuIcon=true"
+            src="https://diogen.netlify.app/chat?hideMenuIcon=true"
             width="508"
             height="552"
             frameborder="0"
